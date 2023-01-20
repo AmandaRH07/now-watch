1434f98a6d1dc3cd8bee0620cf5abd2ff4637086
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2023-01-20 15:06:27 -0300

    Ajustes na sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import './style.css';
+import playBackground from '../../images/playBackground.png'
 
-export default function sidebar(){
+export default function sidebar() {
   return (
     <div className="sidebar-container" >
-      <h2 className="titles-container"> SideBar </h2>
+      <h2 className="titles-container"> Streamings </h2>
+      <div className="options-content">
+        <div className="select-stream">
+          <input type="checkbox" class="radio_item" />
+          <label class="label_item"> <img src={playBackground}/></label>
+          <p className="sidebar-p">Netflix</p>
+        </div>
+        <p className="sidebar-p">Amazon Prime</p>
+        <p className="sidebar-p">HBO Max</p>
+        <p className="sidebar-p">Disney Plus</p>
+        <p className="sidebar-p">Star Plus</p>
+        <p className="sidebar-p">Globo Play</p>
+      </div>
+      <h2>Tipo</h2>
+      <p className="sidebar-p">Filme</p>
+      <p className="sidebar-p">Série</p>
+
+      <h2>Gênero</h2>
+      <p>Ação</p>
+      <p>Animação</p>
+      <p>Comédia</p>
+      <p>Drama</p>
+      <p>Ficção Científica</p>
+      <p>Romance</p>
+      <p>Terror</p>
     </div>
   )
 };
